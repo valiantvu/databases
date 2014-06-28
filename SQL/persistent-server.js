f7aec479a341379bea243ccd412b33aee73aa10d
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-27 17:04:37 -0700

    Add server code

--- SQL/persistent-server.js
+++ SQL/persistent-server.js
@@ -1,5 +1,15 @@
 
 /* You already know how to create an http server from the previous
  * assignment; you can re-use most of that code here. */
+var http = require("http");
+var handleRequest  = require("../server/request-handler.js");
 
+
+var port = 3000;
+
+var ip = "127.0.0.1";
+
+var server = http.createServer(handleRequest.handler);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
 
